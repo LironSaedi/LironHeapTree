d9321369b0b37f37474ab5c88ad102c0f5afcb6a
Lion Saedi <lionsaedi28@outlook.com>
2021-08-04 18:20:18 -0700

    I finished the Heap Treegit status

--- Heap.cs
+++ Heap.cs
@@ -9,12 +9,14 @@ namespace LironHeapTree
     {
         int Capacity;
         int Count;
-
+        bool greaterOrLess;
         T[] items;
-        public Heap(int capacity)
+        Comparer<T> comparer;
+        public Heap(int capacity, Comparer<T> comparer)
         {
             this.Capacity = capacity;
             this.items = new T[capacity];
+            this.comparer = comparer;
         }
 
 
@@ -43,8 +45,9 @@ namespace LironHeapTree
             Count++;
 
             HeapifyUp(Count - 1);
+           
         }
-
+        
         private void Resize()
         {
             T[] largerArray = new T[items.Length * 2];
@@ -58,12 +61,13 @@ namespace LironHeapTree
 
         private void HeapifyUp(int index)
         {
-            if(index == 0)
+            if (index == 0)
             {
                 return;
             }
             int parent = GetParent(index);
-            if (items[index].CompareTo(items[parent]) < 0) //min heap
+           
+            if (comparer.Compare(items[index], items[parent]) < 0) //min heap
             {
                 T holder = items[index];
                 items[index] = items[parent];
@@ -77,6 +81,53 @@ namespace LironHeapTree
             //If necessary swap their values
             //Recursively call heapify up on parent
             //your base case is when your index is at the root (0)
+        }
+
+        public T Pop()
+        {
+            T holder = items[0];
+            items[0] = items[Count - 1];
+            Count--;
+            
+            HeapifyDown(0);
+            return holder;
+        }
+        private void HeapifyDown(int index)
+        {
+            //Lirons Notes
+            //If now left then there is no right.
+            //So we need to check if there is a left if the left is > count
+            // next we need to check if there is a right child bc if there is no right child the smalelr one is the left.
+            // if they both are there then go with the one that is smaller.
+            // and if child is smaller than the item we want swap.
+
+            int indexSwap;
+            int leftChild = GetLeft(index);
+            int rightChild = GetRight(index);
+
+            if (leftChild >= Count)
+            {
+                return;
+            }
+
+            if (rightChild >= Count)
+            {
+                indexSwap = leftChild;
+            }
+
+            else
+            {
+                indexSwap = comparer.Compare(items[leftChild], items[rightChild]) > 0 ? rightChild : leftChild;
+            }
+
+            if (comparer.Compare(items[indexSwap], items[index]) < 0)
+            {
+                T holder = items[index];
+                items[index] = items[indexSwap];
+                items[indexSwap] = holder;
+            }
+
+            HeapifyDown(indexSwap);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,14 +7,31 @@ namespace LironHeapTree
     {
         static void Main(string[] args)
         {
-           // int[] array = new int[] { 1, 9, 2, 13, 10, 3 };
-            Heap<int> heap = new Heap<int>(10);
-            heap.Insert(13);
-            heap.Insert(1);
-            heap.Insert(9);
-            heap.Insert(2);
-            heap.Insert(10);
-            heap.Insert(3);
+            // int[] array = new int[] { 1, 9, 2, 13, 10, 3 };
+            
+            int items = 20;
+            Heap<int> heap = new Heap<int>(items, Comparer<int>.Create((x, y) => y.CompareTo(x))); // min heap to make it max heap just switch  x.CompareTo(y)) to  y.CompareTo(x))
+            int randNum;
+            Random gen = new Random();
+            for (int i = 0; i < items; i++)
+            {
+                randNum = gen.Next(1, 100000);
+                heap.Insert(randNum);
+            }
+
+            for (int i = 0; i < items; i++)
+            {
+                Console.WriteLine(heap.Pop());
+            }
+
+            //Comparer<int> a =Comparer<int>.Create((x, y) => y.CompareTo(x)) ;
+            //Console.WriteLine(a.Compare(10, 11));
+            //heap.Insert(13);
+            //heap.Insert(1);
+            //heap.Insert(9);
+            //heap.Insert(2);
+            //heap.Insert(10);
+            //heap.Insert(3); 
             ;
         }
     }
